a9c0cda8e8370751fbf73dcd1045604767609a67
Ssssanja <61282557+Ssssanja@users.noreply.github.com>
2021-03-13 20:49:39 +0300

    Added @RequestMapping to Class

--- src/main/java/com/zaurtregulov/spring/mvc/MyController.java
+++ src/main/java/com/zaurtregulov/spring/mvc/MyController.java
@@ -5,8 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.servlet.http.HttpServletRequest;
-
+@RequestMapping("/employee")
 @Controller
 public class MyController {
 
@@ -19,24 +18,6 @@ public class MyController {
     public String askEmployeeDetails(){
         return "ask-emp-details-view";
     }
-
-//    @RequestMapping("/showDetails")
-//    public String showEmpDetails(){
-//        return "show-emp-details-view";
-//    }
-
-//    @RequestMapping("/showDetails")
-//    public String showEmpDetails(HttpServletRequest request, Model model){
-//
-//        String empName = request.getParameter("employeeName");
-//        empName = "Mr. " + empName;
-//        model.addAttribute("nameAttribute", empName);
-//        model.addAttribute("description", "udemy Listener");
-//
-//        return "show-emp-details-view";
-//    }
-
-
 
     @RequestMapping("/showDetails")
     public String showEmpDetails(@RequestParam("employeeName") String empName
